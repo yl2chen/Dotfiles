94872c5ec2f8aeb92bbb648c5930e52f6c85e681
Yulin Chen <yulin@Yulins-MBP.lan>
2016-05-02 00:08:25 -0700

    git scm breeze

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+# Vim
 ln -s .vimrc ~/.vimrc
+
+# Scm breeze
+ln -s ~/dotfiles/.scm_breeze ~/.scm_breeze
+~/.scm_breeze/install.sh
